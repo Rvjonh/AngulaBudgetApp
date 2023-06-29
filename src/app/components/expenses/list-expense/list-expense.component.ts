fb29c31ee05f9feaf250455384aa58253417b103
[Rvjonh] <[jonhvelasco3@gmail.com]>
2023-06-29 17:07:10 -0400

    feat: observable in budgetService added

--- src/app/components/expenses/list-expense/list-expense.component.ts
+++ src/app/components/expenses/list-expense/list-expense.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'app-list-expense',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-expense.component.css']
 })
 export class ListExpenseComponent {
+
+  constructor(private _budgetService: BudgetService){
+    this._budgetService.getExpenses().subscribe((data)=>{
+      console.log(data)
+    })
+  }
 
 }
--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ExpenseModel } from '../models/expense';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,16 @@ import { ExpenseModel } from '../models/expense';
 export class BudgetService {
   budget:number = 0;
   rest:number = 0;
+  private expenses$ = new Subject<ExpenseModel>();
   
   constructor() { }
 
   addExpense(expense:ExpenseModel){
     this.rest -= expense.quantity;
+    this.expenses$.next(expense);
+  }
+
+  getExpenses():Observable<any>{
+    return this.expenses$.asObservable();
   }
 }
