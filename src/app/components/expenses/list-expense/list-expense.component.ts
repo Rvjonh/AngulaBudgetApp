354495f70aa0afb4746371bd2a360f362ec6d308
[Rvjonh] <[jonhvelasco3@gmail.com]>
2023-06-30 14:57:03 -0400

    feat: list expenses

--- src/app/components/expenses/list-expense/list-expense.component.ts
+++ src/app/components/expenses/list-expense/list-expense.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ExpenseModel } from 'src/app/models/expense';
 import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
@@ -8,10 +10,29 @@ import { BudgetService } from 'src/app/services/budget.service';
 })
 export class ListExpenseComponent {
 
+  subcription: Subscription;
+  budget:number = 0;
+  rest:number = 0;
+  listExpenses: ExpenseModel[] = [];
+
   constructor(private _budgetService: BudgetService){
-    this._budgetService.getExpenses().subscribe((data)=>{
-      console.log(data)
+    this.subcription = this._budgetService.getExpenses().subscribe((data)=>{
+      this.rest -= data.quantity;
+      this.listExpenses.push(data);
     })
+  }
+
+  ngOnInit():void{
+    this.budget = this._budgetService.budget;
+    this.rest = this._budgetService.rest;
+  }
+
+  ngOnDestroy():void{
+    this.subcription.unsubscribe()
+  }
+
+  getTotalQuantityFromExpenses(){
+    return this.listExpenses.reduce((sum, item)=> sum+=item.quantity, 0);
   }
 
 }
